0056409fc1bf6c8640cff752339e9b342c06787e
BHAV0207 <bhavyajain4274@gmail.com>
2024-11-11 10:40:52 +0530

    employee ui created

---
+++ ems/src/EmployeeComponents.jsx/TaskStatusList.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function TaskStatusList() {
+  return (
+    <div className='flex mt-10 items-center justify-between gap-5'>
+      <div className='bg-blue-500 py-6 px-9 w-[45%] rounded-md'>
+        <h1 className='font-semibold text-3xl'>0</h1>
+        <h1 className='font-medium text-2xl'>New Task</h1>
+      </div>
+      <div className='bg-green-500 py-6 px-9 w-[45%] rounded-md'>
+        <h1 className='font-semibold text-3xl'>0</h1>
+        <h1 className='font-medium text-2xl'>Comeleted Task</h1>
+      </div>
+      <div className='bg-red-500 py-6 px-9 w-[45%] rounded-md'>
+        <h1 className='font-semibold text-3xl'>0</h1>
+        <h1 className='font-medium text-2xl'>Accepted Task</h1>
+      </div>
+      <div className='bg-yellow-500 py-6 px-9 w-[45%] rounded-md'>
+        <h1 className='font-semibold text-3xl'>0</h1>
+        <h1 className='font-medium text-2xl'>Failed Task</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default TaskStatusList
--- ems/src/Pages/Employee.jsx
+++ ems/src/Pages/Employee.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../CommonComponents/Header'
+import TaskStatusList from '../EmployeeComponents.jsx/TaskStatusList'
+import TasksList from '../EmployeeComponents.jsx/TasksList'
 
 function Employee() {
   return (
    <div className='p-10'>
       <Header></Header>
+      <TaskStatusList></TaskStatusList>
+      <TasksList></TasksList>
    </div>
   )
 }
