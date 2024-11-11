71703261a7b66a8a91d8544fa06b9db5701bb14a
BHAV0207 <bhavyajain4274@gmail.com>
2024-11-11 11:58:03 +0530

    page rendering functionality is done

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,16 +1,34 @@
+import { useState } from 'react'
 import './App.css'
 import Admin from './Pages/Admin'
 import Employee from './Pages/Employee'
 import Login from './Pages/Login'
 
 function App() {
+
+  let [user , setUser] = useState(null)
+
+  const Logging = (email , password) => {
+    console.log(email)
+    console.log(password)
+    if(email == 'admin@gmail.com' && password == '123'){
+      setUser('admin')
+    }
+    else if(email == 'e@e.com' && password == '123'){
+      setUser('employee')
+    }
+    else{
+      alert('wrong crediantials');
+    }
+
+
+  }
   
 
   return (
     <>
-      {/* <Login></Login> */}
-      {/* <Employee></Employee> */}
-      <Admin></Admin>
+    {!user ? <Login login={Logging}></Login> : ''}
+    {user == 'admin' ? <Admin></Admin> : (user == 'employee' ? <Employee></Employee> : null)}
     </>
   )
 }
--- ems/src/Pages/Login.jsx
+++ ems/src/Pages/Login.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login() {
+function Login({ login }) {
+  let [email , setEmail] = useState('');
+  let [pass , setPass] = useState('');
+
+  const submittingForm = (e) => {
+    e.preventDefault();
+    login(email , pass);
+
+    setEmail("")
+    setPass("")
+  }
+
   return (
     <div className="flex h-screen w-screen justify-center items-center">
       <div className="border-emerald-500 border-2 p-20">
-        <form className="flex flex-col justify-center items-center">
-          <input type="email" placeholder="enter your email"  className="bg-transparent border-2 border-emerald-500 outline-none mx-5 py-2 px-4 rounded-md"/>
-          <input type="password"  placeholder="enter your password" className="bg-transparent border-2 border-emerald-500 outline-none m-5 py-2 px-4 rounded-md"/>
-          <button className="bg-emerald-500 outline-none py-2 px-5 rounded-md text-black">Login</button>
+        <form onSubmit={submittingForm} className="flex flex-col justify-center items-center">
+          <input
+            type="email"
+            placeholder="enter your email"
+            className="bg-transparent border-2 border-emerald-500 outline-none mx-5 py-2 px-4 rounded-md"
+            value={email}
+            onChange={(e) => (setEmail(e.target.value))}
+          />
+          <input
+            type="password"
+            placeholder="enter your password"
+            className="bg-transparent border-2 border-emerald-500 outline-none m-5 py-2 px-4 rounded-md"
+            value={pass}
+            onChange={(e) => (setPass(e.target.value))}
+          />
+          <button className="bg-emerald-500 outline-none py-2 px-5 rounded-md text-black">
+            Login
+          </button>
         </form>
       </div>
     </div>
